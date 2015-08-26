e203f8857cd05a981cb9f096fefea74a138c8125
joshuaebowling <joshuaebowling@gmail.com>
2015-08-26 13:31:35 -0400

    removed unused variables, added documentation

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,34 +1,37 @@
-﻿
-var enterKey, $, backbone, ko, async;
+﻿/**
+ * @fileOverview requires base components, 
+ * @author Josh Bowling
+ * @version 0.0.1
+ */
+
+var $, backbone, ko;
 
 $ = window.$ = require('../bower/jquery/dist/jquery.js');
 ko = require('../bower/knockout/dist/knockout.js');
+backbone = require('backbone');
+// set the backbone jQuery value
+backbone.$ = $;
+
+// require all knockout components
 require('./components/appError');
 require('./components/verses');
 require('./components/verse');
 require('./components/bookNavigator');
 require('./components/chaptersNavigator');
 require('./components/versesInput');
-backbone = require('backbone');
-backbone.$ = $;
-async = require('async');
 
-// require('../bower/bootstrap/dist/js/bootstrap.min.js');
+// require all binding handlers
 ko.bindingHandlers.enterKey = require('./bindingHandlers/enterKey.js');
 ko.bindingHandlers.showFocus = require('./bindingHandlers/showFocus.js');
 require('./components/customLoader.js');
-// this would be abstracted by monologue
+
+// run setup
 (function ($) {
     var $d, init, AppViewModel;
     $d = $(document);
     AppViewModel = function() {
-        var self, bookWorker;
+        var self;
         self = this;
-        self.bookWorker = require('./observers/Book');
-        bookWorker = self.bookWorker().retrieve();
-        bookWorker.subscriber('#error', function(data, env) {
-            console.log('error', arguments);
-        });
     };
     init = function () {
         var appViewModel;
