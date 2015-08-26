ed7e4d7303a98e0993c257825a12ca62cc9d78ac
joshuaebowling <joshuaebowling@gmail.com>
2015-08-26 13:14:00 -0400

    code cleanup, documentation, identification of changes to be made

--- public/javascripts/observers/Book.js
+++ public/javascripts/observers/Book.js
@@ -1,4 +1,9 @@
-var BookModel, monologue, Worker, worker,_ ,Response, ko, Worker, util, tmplEmission, STATUS;
+/**
+ * @fileOverview Observer, i.e. the monologue abstraction between the knockout view models and backbone models/colleccitons
+ * @author <a href="mailto:joshuaebowling@gmail.com">Josh Bowling</a>
+ * @version 0.0.1
+ */
+var BookModel, monologue, Worker, worker,_ ,Response, ko, Worker, util;
 
 util = require('util');
 
@@ -6,25 +11,11 @@ _ = require('underscore');
 
 ko = require('../../bower/knockout/dist/knockout.debug.js');
 
-STATUS = {
-	'start':0,
-	'complete':1,
-	'error':2,
-	'invalid':3
-};
+monologue = require('monologue.js');
 
 Response = require('../utility/ResponseObject.js');
 
 BookModel = require('../models/Book');
-
-tmplEmission = function(model, property, action, state) {
-	var tmpl, result;
-	
-	tmpl = '%s.%s.%s.%s';
-	result = util.format(tmpl, model, property, action, state);
-
-	return result;
-};
 
 Worker = function(name) {
 	var self;
@@ -32,23 +23,25 @@ Worker = function(name) {
 	self.name = name;	
 };
 
-monologue = require('monologue.js');
-///CRUD TYPES
-
 Worker.prototype = monologue.prototype;
 
 
 Worker.prototype.create = function(name) {
-	var self, book, workerFunctions, model, WorkerPropertyObservables;
-	// model is the intercept value for abstracting the Factory out
+	var self, book, workerFunctions, WorkerPropertyObservables;
+
 	self = this;
-	// for empty chapters
+	// this the backbone model that contains the name, chapters, verses
 	book = new BookModel(name, null);
-	window.book = book;
-	// assumes model/self since its operating in same context
+
+/** iteratively creates computed observables from backbone model attributes
+	* @todo pull keys value from the model. model attributes is pulling back more than name/chapters at present
+	* @todo investigate where this code is actually being used
+*/
 	WorkerPropertyObservables = function() {
+		// assumes model/self since its operating in same context
 		var keys, workerProperties, currentKey;
-		// pull this info from model attributes
+		/* pull this info from model attributes
+		*/
 		keys = ['name', 'chapters'];
 		workerProperties = {};
 		_.each(keys, function(key, keyi) {
@@ -71,78 +64,108 @@ Worker.prototype.create = function(name) {
 		});
 		return workerProperties;
 	};
-	// set worker to book
-	model = book;
-	// worker functions create an abstraction layer between model and returned object
+	/** worker functions create an abstraction layer between model and returned object. this is the object that's given to the knockout side of the app 
+	* @todo workerFunctions may need to be bifurcated because the observables contained herein aren't worker functions
+	*/
 	workerFunctions = {
+		/* currently not implemented */
 		delete: function() {
 			self.emit('crud.delete.done', new Response(null, 200, null));
 		},
 		addChapter: function() {
 			var chapters, newChapter;
+
 			chapters = book.get('chapters');
 			newChapter = new chapters.model(chapters.models.length + 1);
 			chapters.add(newChapter);
-			console.log(newChapter);
 			self.emit('chapters.crud.create.done', new Response(newChapter, 200, book.get('chapters')));
 
 			return newChapter;
 		},
 		setCurrentChapter: function(num) {
 			var chapter, chapters;
+
+			// Check to see if chapter exists also
+			if(!num || !_.isNumber(num)) {
+				return self.emit('book.chapters.chapter.set.error', new Response(null, 404, null));
+			}
+
 			chapter = book.get('chapters').findWhere({num: num});
 			chapters = book.get('chapters');
 			workerFunctions.currentChapter(num);
 			self.emit('chapters.chapter.set', new Response(chapter, 200, chapters));
 		},
+		/** Set the book's name
+		* @param {string} name
+		*/
 		setName: function(name) {
-			console.log(name);
 			if(!name || !_.isString(name)) {
 				return self.emit('book.name.set.error', new Response(null, 404, null));
 			}
+
 			book.set('name', name);
 			self.emit('book.name.set', new Response(name, 200, book));
 		},
-		currentChapter: ko.observable(0),
-		currentChapterObject: ko.pureComputed(function() {
-			return book.get('chapters').findWhere({num: workerFunctions.currentChapter()});
-		}),
-		isCreated: ko.observable(false),
+		/** Add a verse to currently selected chapter
+		* @param {string} verseText
+		*/		
 		addVerse: function(verseText) {
 			var chapters, chapter, verses, verse, num;
+
 			chapter = self.workerFunctions.currentChapterObject();
 			if(!chapter) {
-//				chapter = book.addChapter();
-//				console.log('noChapter', book.get('chapters').toJSON());
 				return self.emit('chapters.chapter.verse.crud.create.error', new Response(null, 404, null));
 			}
+
 			verses = chapter.get('verses');
 
 			if(!_.isString(verseText) || verseText === '') {
 				return self.emit('chapters.chapter.verse.crud.create.error.noVerse', new Response(null, 404, null));
 			}
+
 			verse = chapter.addVerse(verseText);
 			self.emit('chapters.chapter.verse.crud.create.done', new Response(verse, 200, verses));
 		},
+		// model.attribute-based computed-s
 		attributes: WorkerPropertyObservables(),
+		// abstraction of monologue for use in view models
 		subscriber: _.bind(self.on, self),
-		publisher: _.bind(self.emit, self)
+		// abstraction of monologue for use in view models
+		publisher: _.bind(self.emit, self),
+		/**
+		* hold the current selected chapter... there is tension between this and setCurrentChapter.
+		* @todo research which function (this or setCurrentChapter) is being used in the code more effectively and eliminate the other
+		* @param {number}
+		*/
+		currentChapter: ko.observable(0),
+		/**
+		* @todo - remove from implementation
+		*/
+		currentChapterObject: ko.pureComputed(function() {
+			return book.get('chapters').findWhere({num: workerFunctions.currentChapter()});
+		}),
+		/**
+		* @param {bool}
+		* @todo remove from implementation
+		*/
+		isCreated: ko.observable(false)
 	};
 	self.workerFunctions = workerFunctions;
 	// emit the change to worker who will propogate the rest
 	self.workerFunctions.currentChapter.subscribe(function(val) {
 		self.workerFunctions.setCurrentChapter(val);
 	});
+	// emit the workerFunctions to the listeners
 	self.emit('create.done', { book: workerFunctions });
 	// change the chapter from 0 to 1
 	self.workerFunctions.setCurrentChapter(1);
 	return self;
 };
-
+/** used by knockout side to retrieve worker if the create.done event has already fired */
 Worker.prototype.retrieve = function() {
 	return worker.workerFunctions;
 };
-
+// create the worker if there's not one alrady (singleton)
 module.exports = function() { 
 	if(!worker){
 		worker = new Worker();
