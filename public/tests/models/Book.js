2a1683082cd7304dbf13d5b20365d9f9cf608dad
joshuaebowling <joshuaebowling@gmail.com>
2015-09-03 20:30:24 -0400

    expanded tests. Will continue to expand test coverage before fully converting to grunt from gulp to make sure testing solutionworks as expected

--- public/tests/models/Book.js
+++ public/tests/models/Book.js
@@ -1,22 +1,42 @@
-var testData, resultData, _, should;
+var testData, resultData, _, should, Book;
 
 _ = require('underscore');
 should = require('should');
+Book = require('../../javascripts/models/Book');
 
 testData = require('../data/Book');
+
 resultData = {
 	verses: []
 };
 
 describe('Book Model Tests', function() {
 	it('Initialize a Verse, check values from constructor invocation, re-set, check again', function() {
-		var self, book;
+		var self, book, bookName, newBookName, chapters;
 		self = this;
-		self.Book = require('../../javascripts/models/Book');
-		self.Book.should.be.a.Function;
-		book = new self.Book(testData.name);
+		newBookName = 'Song Book';
+		Book.should.be.a.Function;
+		book = new Book(testData.Book.name);
 		book.should.be.an.Object;
+		book.addChapter.should.be.a.Function();
+		bookName = book.get('name');
+		bookName.should.eql(testData.Book.name);
+		// change the book name
+		book.set('name', newBookName);
+		bookName = book.get('name');
+		bookName.should.eql(newBookName);
+		chapters = book.get('chapters');
+		chapters.should.be.okay;
+		chapters.should.be.an.Object;
 	});
 
-
+	it('Add a chapter to a book', function() {
+		var book, newChap, chapters;
+		book = new Book();
+		newChap = book.addChapter();
+		newChap.should.be.okay;
+		chapters = book.get('chapters');
+		chapters.length.should.be.okay;
+		chapters.length.should.eql(2);
+	});
 });
